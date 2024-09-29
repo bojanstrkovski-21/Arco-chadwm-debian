19a95806a3689b6b161fd9ed3111f67eb95185bf
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2024-09-29 18:56:12 +0200

    Create sparklines.sh

---
+++ debian-install/sparklines.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+# installing sparklines/spark
+sudo sh -c "curl https://raw.githubusercontent.com/holman/spark/master/spark -o /usr/local/bin/spark && chmod +x /usr/local/bin/spark"
