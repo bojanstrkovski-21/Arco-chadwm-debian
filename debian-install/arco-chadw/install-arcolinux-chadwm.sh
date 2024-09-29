69fa3eaec2fc59352eb705f3315d7aa7bdaa602e
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2024-09-29 14:06:55 +0200

    Update install-arcolinux-chadwm.sh

--- debian-install/arco-chadw/install-arcolinux-chadwm.sh
+++ debian-install/arco-chadw/install-arcolinux-chadwm.sh
@@ -15,17 +15,33 @@ sudo cp arcolinux-chadwm/usr/bin/exec-chadwm /usr/bin
 sudo cp arcolinux-chadwm/usr/share/xsessions/chadwm.desktop /usr/share/xsessions
 cp -r arcolinux-chadwm/etc/skel/.bin ~
 cp -r arcolinux-chadwm/etc/skel/.config ~
-#rm ~/.config/arco-chadwm/scripts/bar.sh
-#rm ~/.config/arco-chadwm/scripts/run.sh
-cp ~/Templates/debian-install/arco-chadw/run.sh  ~/.config/arco-chadwm/scripts
-cp ~/Templates/debian-install/arco-chadw/bar.sh ~/.config/arco-chadwm/scripts
-#rm ~/.config/arco-chadwm/picom/picom.conf
-cp ~/Templates/debian-install/arco-chadw/picom.conf  ~/.config/arco-chadwm/picom
-cp ~/Templates/debian-install/arco-chadw/config.def.h ~/.config/arco-chadwm/chadwm
-cp ~/Templates/debian-install/arco-chadw/sxhkdrc  ~/.config/arco-chadwm/sxhkd
-cp ~/Templates/debian-install/arco-chadw/uca.xml ~/.config/Thunar/
 
-exit 1
+# overwriting the official code from ArcoLinux with my own
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+#cp ~/Templates/debian-install/arco-chadw/run.sh  ~/.config/arco-chadwm/scripts
+cp run.sh  ~/.config/arco-chadwm/scripts
+
+#cp ~/Templates/debian-install/arco-chadw/bar.sh ~/.config/arco-chadwm/scripts
+cp bar.sh ~/.config/arco-chadwm/scripts
+
+#cp ~/Templates/debian-install/arco-chadw/picom.conf  ~/.config/arco-chadwm/picom
+cp picom.conf  ~/.config/arco-chadwm/picom
+
+
+#cp ~/Templates/debian-install/arco-chadw/config.def.h ~/.config/arco-chadwm/chadwm
+cp config.def.h ~/.config/arco-chadwm/chadwm
+
+#cp ~/Templates/debian-install/arco-chadw/sxhkdrc  ~/.config/arco-chadwm/sxhkd
+cp sxhkdrc  ~/.config/arco-chadwm/sxhkd
+
+#cp ~/Templates/debian-install/arco-chadw/uca.xml ~/.config/Thunar/
+[ -d $HOME"/.config/Thunar" ] || mkdir -p $HOME"/.config/Thunar"
+cp uca.xml ~/.config/Thunar/
+
+# build and install chadwm
 cd ~/.config/arco-chadwm/chadwm/
 sudo make install
+
+# removing this package - it slows down terminals and thunar
+sudo apt remove -y xdg-desktop-portal-gnome
 
