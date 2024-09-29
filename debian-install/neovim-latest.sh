da511146fc75720d2712b2fc7fe5284964a52d64
bojanstrkovski-21 <115637403+bojanstrkovski-21@users.noreply.github.com>
2024-09-29 13:31:24 +0200

    Update neovim-latest.sh

--- debian-install/neovim-latest.sh
+++ debian-install/neovim-latest.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-# wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz ~/Downloads
+# Define the tools to check/install
+TOOLS=(git wget curl)
 
+# Loop through each tool
+for TOOL in "${TOOLS[@]}"; do
+  # Check if tool is installed
+  if ! command -v $TOOL &> /dev/null; then
+    echo "Tool: $TOOL is not installed. Installing..."
+    sudo apt-get update && sudo apt-get install -y $TOOL
+  fi
+done
+
+echo "Installation complete."
+
+# download and install latest neovim
 cd ~/Downloads
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-echo "export PATH="/opt/nvim-linux64/bin:$PATH"" >> ~/.bashrc
+echo 'export PATH="/opt/nvim-linux64/bin:${PATH}"' >> ~/.bashrc
 
 source ~/.bashrc
